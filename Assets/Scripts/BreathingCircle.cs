15645cec3d500f21a50df5fd40dc2c5be09521f5
Ervinovich <ehamul15@student.aau.dk>
2016-04-28 16:48:20 +0200

    Breathing Circle + Follow the leader
    Code for Companions following the main charecter included with the code
    for breathing cycle

--- Assets/Scripts/BreathingCircle.cs
+++ Assets/Scripts/BreathingCircle.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class BreathingCircle : MonoBehaviour {
 
-    public int DelayInhale;
-    public int DelayExhale;
-    public int HoldBreath;
+    public float InhaleTime = 3;
+    public float ExhaleTime = 6;
+    public float HoldBreath = 1;
+    public float InhaleSizeShift = 0.01f;
+    public float ExhaleSizeshift = 0.005f;
     bool iCanInhale;
     bool iCanExhale;
-    bool iCanHold;
-    bool iCanHoldE;
 
     // Use this for initialization
     void Start()
@@ -20,19 +20,15 @@ public class BreathingCircle : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+         if(iCanInhale == true)
+        {
+            this.transform.localScale += new Vector3(InhaleSizeShift, InhaleSizeShift, 0f);
+        }
+         else if(iCanExhale == true)
+        {
+            this.transform.localScale += new Vector3(-ExhaleSizeshift, -ExhaleSizeshift, 0f);
+        }
 
-        if (iCanInhale == true && iCanHold == true)
-        {
-            StopCoroutine(Exhale());
-            StartCoroutine(Inhale());
-            iCanHold = false;
-        }
-        else if (iCanExhale == true && iCanHoldE == true)
-        {
-            StopCoroutine(Inhale());
-            StartCoroutine(Exhale());
-            iCanHoldE = false;
-        }
     }
 
     public IEnumerator Controller()
@@ -42,42 +38,18 @@ public class BreathingCircle : MonoBehaviour {
 
             print("Heyoo");
             iCanInhale = true;
-            iCanHold = true;
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(InhaleTime);
             iCanInhale = false;
             print("Holding Breath");
-            yield return new WaitForSeconds(1f);
-            print("I will Exhale now");
-            iCanHoldE = true;
+            yield return new WaitForSeconds(HoldBreath);
             iCanExhale = true;
-            yield return new WaitForSeconds(6f);
-            iCanExhale = false;
+            print("I will Exhale now");
+            yield return new WaitForSeconds(ExhaleTime);
             print("I have Exhaled");
+            iCanExhale = false;
             yield return new WaitForSeconds(1f);
 
         }
     }
-
-    public IEnumerator Inhale()
-    {
-        while (true)
-        {
-            print("IIIIIINHALING");
-            this.transform.localScale += new Vector3(0.1f, 0.1f, 0f);
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-
-    public IEnumerator Exhale()
-    {
-        while (true)
-        {
-            print("Exhaaaaaling");
-            this.transform.localScale -= new Vector3(0.05f, 0.05f, 0f);
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-
-
 }
 
---
+++ Assets/Scripts/CompanionFollow.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompanionFollow : MonoBehaviour {
+
+    private Vector2 charecterLocation;
+    private float moveSpeed = 0f;
+    private float playerX;
+    private float playerY;
+    bool attached2Player = false;
+    bool greetingPlayer = false;
+    public float offSetX;
+    public float offSetY;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (attached2Player == true)
+        {
+            moveSpeed = 0.12f;
+        }
+        else if (greetingPlayer == true)
+        {
+            moveSpeed = 0.001f;
+        }
+        else
+        {
+            moveSpeed = 0f;
+        }
+
+        playerX = GameObject.Find("Charecter").transform.position.x;
+        playerY = GameObject.Find("Charecter").transform.position.y;
+
+        charecterLocation = new Vector3(playerX+offSetX, playerY+offSetY, 0.0f);
+
+        transform.position = Vector2.Lerp(transform.position, charecterLocation, moveSpeed);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.name == "Charecter")
+        {
+            attached2Player = true;
+        }
+        else if (hitInfo.name == "GreetingZone")
+        {
+            greetingPlayer = true;
+        }
+    }
+}
