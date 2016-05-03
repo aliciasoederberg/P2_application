31c5f677005cd81d9a88138e165b3362c360b5de
Ervinovich <ehamul15@student.aau.dk>
2016-05-03 12:41:36 +0200

    Fixed rotation
    Not pretty in code terms but it works

--- Assets/Scripts/CompanionFollow.cs
+++ Assets/Scripts/CompanionFollow.cs
@@ -3,24 +3,43 @@ using System.Collections;
 
 public class CompanionFollow : MonoBehaviour {
 
-    private Vector2 charecterLocation;
+    private Vector3 charecterLocation;
     private float moveSpeed = 0f;
     private float playerX;
     private float playerY;
+    GameObject parent;
+    GameObject child;
     bool attached2Player = false;
     bool greetingPlayer = false;
+    bool readyToParent = true;
     public float offSetX;
     public float offSetY;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public KeyCode MoveLeft;
+    public KeyCode MoveRight;
+    Rigidbody2D rb2d;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        child = GameObject.Find("Companion (2)");
+        parent = GameObject.Find("Charecter");
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        if(transform.position == charecterLocation)
+        {
+            print("Kebab jeg er der nu");
+            //attached2Player = false;
+            readyToParent = false;
+            child.transform.parent = parent.transform;
+        }
+
 
         if (attached2Player == true)
         {
@@ -35,12 +54,37 @@ public class CompanionFollow : MonoBehaviour {
             moveSpeed = 0f;
         }
 
-        playerX = GameObject.Find("Charecter").transform.position.x;
-        playerY = GameObject.Find("Charecter").transform.position.y;
+        if (readyToParent == true)
+        {
+            playerX = GameObject.Find("Charecter").transform.position.x;
+            playerY = GameObject.Find("Charecter").transform.position.y;
+            charecterLocation = new Vector3(playerX + offSetX, playerY + offSetY, 0.0f);
+            transform.position = Vector2.Lerp(transform.position, charecterLocation, moveSpeed);
+        }
+        else
+        {
+            if (Input.GetKey(moveUp))
+            {
+                rb2d.velocity = new Vector2(0, 1);
+            }
+            else if (Input.GetKey(moveDown))
+            {
+                rb2d.velocity = new Vector2(0, -1);
+            }
+            else if (Input.GetKey(MoveLeft))
+            {
+                rb2d.velocity = new Vector2(-1, 0);
+            }
+            else if (Input.GetKey(MoveRight))
+            {
+                rb2d.velocity = new Vector2(1, 0);
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(0, 0);
+            }
+        }
 
-        charecterLocation = new Vector3(playerX+offSetX, playerY+offSetY, 0.0f);
-
-        transform.position = Vector2.Lerp(transform.position, charecterLocation, moveSpeed);
     }
 
 
