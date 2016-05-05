2135c588b9ff8ab0dfbb1d5fdeed649bc8364277
aliciasoederberg <esader15@student.aau.dk>
2016-05-05 15:18:46 +0200

    BG sound and destroyer!
    spawner in progress

--- Assets/Scripts/CollisionSound.cs
+++ Assets/Scripts/CollisionSound.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class CollisionSound : MonoBehaviour {
 
     public AudioSource Sound;
+    public float destructionTimer = 10.0f;
 
- 
+
+
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         if (hitInfo.name == "Character")
         {
-        Sound.Play();   
+            Sound.Play();
+            GameObject.Destroy(gameObject, destructionTimer);
         }
     }
 }
---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjects : MonoBehaviour {
+
+    public Transform[] spawnObjects;
+    public float spawnTime = 4.0f;
+    public GameObject interactiveObjects;
+    //public GameObject[] Rings;
+
+    // Use this for initialization
+    void Start()
+    {
+        //InvokeRepeating("spawnItems", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawnItems()
+    {
+        int spawnIndex = Random.Range(0, spawnObjects.Length);//sets index of array randomly
+        Instantiate(interactiveObjects, spawnObjects[spawnIndex].position, spawnObjects[spawnIndex].rotation);
+    }
+}
